7e1c52ea12eec160f075d353542a10d43759b9b1
Affan Raza <razaaffan08@gmail.com>
2026-03-27 16:48:15 +0530

    New changes

---
+++ src/router.ts
@@ -0,0 +1,16 @@
+
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
